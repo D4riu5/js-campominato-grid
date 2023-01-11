058b5b55e70fb6b365dc9726a07bc25f355e0877
D4riu5 <R3iji94@gmail.com>
2023-01-11 23:15:52 +0100

    completed project

--- js/script.js
+++ js/script.js
@@ -1,13 +1,39 @@
 const gridContainer = document.getElementById('grid-container');
+const startGame = document.getElementById('start-game');
+const resetGame = document.getElementById('reset-game');
+
+let cellsCreated = false;
+
+startGame.addEventListener ('click',
+    function(){
+        if (!cellsCreated) {
+            for (let i = 0; i < 100; i++) {
+
+                const newCell = createNewCell(i);
+                gridContainer.append(newCell);
+            }
+            cellsCreated = true;
+        }
+    }
+);
+
+resetGame.addEventListener ('click',
+    function () {
+        while (gridContainer.firstChild) {
+            gridContainer.removeChild(gridContainer.firstChild);
+        }
+        cellsCreated = false;
+    }
+);
 
 
-function createNewCell() {
+function createNewCell(i) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
+    cell.innerHTML = i + 1; // Add the progressive number
     cell.addEventListener('click',
         function () {
-            console.log(this);
-            console.log(this.classList);
+            console.log("cell " + this.innerText);
 
             if(this.classList.contains('clicked')){
                 this.classList.remove('clicked');
@@ -20,9 +46,3 @@ function createNewCell() {
     return cell;
 }
 
-for (let i = 0; i < 100; i++) {
-
-    const newCell = createNewCell();
-    gridContainer.append(newCell);
-
-}
